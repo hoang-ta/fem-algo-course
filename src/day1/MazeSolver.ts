26b3133dc049a3c582d05d4af92887d192a5051b
Hoang Ta <huyhoang.ta97@gmail.com>
2024-10-02 16:16:51 +0700

    2nd way

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,58 +1,110 @@
+const direction = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+];
+
 export default function solve(
     maze: string[],
     wall: string,
     start: Point,
     end: Point,
 ): Point[] {
-    const resut = findPath(maze, wall, start, end, {});
-    console.log("result: ", resut);
-    return resut;
+    /** Functional style */
+    // const path = findPath(maze, wall, start, end, {});
+    // console.log("result: ", path);
+    // return path;
+
+    /** Another method */
+    const visited: boolean[][] = [];
+    for (let i = 0; i < maze.length; i++) {
+        visited.push(new Array(maze[0].length).fill(false));
+    }
+    const path: Point[] = [];
+    walk(maze, wall, start, end, visited, path);
+    return path;
 }
+
+const walk = (
+    maze: string[],
+    wall: string,
+    current: Point,
+    end: Point,
+    visited: boolean[][],
+    path: Point[],
+): boolean => {
+    //base
+    const isOffMap =
+        current.x === maze[0].length ||
+        current.x < 0 ||
+        current.y === maze.length ||
+        current.y < 0;
+    if (isOffMap) return false;
+    const isWall = maze[current.y][current.x] === wall;
+    if (isWall) return false;
+    const isVisited = visited[current.y][current.x];
+    if (isVisited) return false;
+    const isDone = current.x === end.x && current.y === end.y;
+    if (isDone) {
+        path.push(current);
+        return true;
+    }
+    //pre
+    visited[current.y][current.x] = true;
+    path.push(current);
+    //recursion
+    for (let i = 0; i < direction.length; i++) {
+        if (
+            walk(
+                maze,
+                wall,
+                {
+                    x: current.x + direction[i][0],
+                    y: current.y + direction[i][1],
+                },
+                end,
+                visited,
+                path,
+            )
+        ) {
+            return true;
+        }
+    }
+    //post
+    return false;
+};
 
 function findPath(
     maze: string[],
     wall: string,
-    start: Point,
+    current: Point,
     end: Point,
     visitedDict: Record<string, Point>,
 ): Point[] {
-    const isValid = checkValidSlot(maze, wall, start, visitedDict);
+    const isValid = checkValidSlot(maze, wall, current, visitedDict);
     if (!isValid) return [];
-    const pointId = getPointId(start);
-    visitedDict[pointId] = start;
-    const isFinish = start.x === end.x && start.y === end.y;
+    const pointId = getPointId(current);
+    visitedDict[pointId] = current;
+    const isFinish = current.x === end.x && current.y === end.y;
     if (isFinish) {
         return Object.values(visitedDict);
     }
-    const path1 = findPath(
-        maze,
-        wall,
-        { ...start, x: start.x + 1 },
-        end,
-        visitedDict,
-    );
-    const path2 = findPath(
-        maze,
-        wall,
-        { ...start, x: start.x - 1 },
-        end,
-        visitedDict,
-    );
-    const path3 = findPath(
-        maze,
-        wall,
-        { ...start, y: start.y + 1 },
-        end,
-        visitedDict,
-    );
-    const path4 = findPath(
-        maze,
-        wall,
-        { ...start, y: start.y - 1 },
-        end,
-        visitedDict,
-    );
-    return [...path1, ...path2, ...path3, ...path4];
+    const path: Point[] = [];
+    for (let i = 0; i < direction.length; i++) {
+        const subPath = findPath(
+            maze,
+            wall,
+            {
+                x: current.x + direction[i][0],
+                y: current.y + direction[i][1],
+            },
+            end,
+            visitedDict,
+        );
+        path.push(...subPath);
+    }
+    return path;
 }
 
 const checkValidSlot = (
